78db3ab90bb07fcbf54b8f291f1a4d8b9613f050
quadseed <contact@quadseed.net>
2022-06-25 19:21:42 +0900

    Edit: change watch page components

--- components/watch/Comments.tsx
+++ components/watch/Comments.tsx
@@ -1,8 +1,18 @@
+import { AdjustmentsIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { testData } from '../test'
 
 function Comments() {
   return (
-    <div>Comments</div>
+    <div>
+      <div className='flex items-center mx-3 my-4 pb-3 space-x-2 p-1'>
+        <p>{testData.commentCount.toLocaleString()} Comments</p>
+        <p className='flex items-center px-4 space-x-2 select-none hover:cursor-pointer active:bg-slate-200 active:rounded-full'><AdjustmentsIcon className='h-6 w-6 text-gray-500' />Sort</p>
+      </div>
+      <div>
+        hogehoge
+      </div>
+    </div>
   )
 }
 
--- components/watch/Description.tsx
+++ components/watch/Description.tsx
@@ -9,7 +9,7 @@ function Description() {
       <div className='flex items-center px-2 py-1'>
         <Link href="/channel"><img className='flex h-14 w-14 rounded-full hover:cursor-pointer' src={testData.icon} /></Link>
         <div className='px-3'>
-          <p className='text-sm font-bold hover:underline hover:backdrop:cursor-pointer'><Link href="/channel">{testData.channelName}</Link></p>
+          <p className='text-sm font-bold hover:underline hover:cursor-pointer'><Link href="/channel">{testData.channelName}</Link></p>
           <p className='text-xs'>{testData.subscriber}</p>
         </div>
       </div>
