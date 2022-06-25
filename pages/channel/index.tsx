dbb9b76f1bb64d95cd87050a225fec1b54d3fd3f
quadseed <contact@quadseed.net>
2022-06-26 04:03:10 +0900

    Add: channel/index.tsx

--- pages/channel/index.tsx
+++ pages/channel/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
+import Header from '../../components/Header';
 
 
 const Index: NextPage = () => {
@@ -7,8 +8,10 @@ const Index: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Archive-Player</title>
+        <title>Channel List - Archive-Player</title>
       </Head>
+      
+      <Header />
       Index
     </div>
   )
