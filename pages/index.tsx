ad5a979fad1ca934ec9892c60274e1906079aa1d
quadseed <contact@quadseed.net>
2022-06-26 15:51:34 +0900

    Edit: add temp Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
@@ -9,7 +10,12 @@ const Home: NextPage = () => {
         <title>Archive-Player</title>
       </Head>
       <Header />
-      Home
+
+      <div className='mx-60 my-40 space-y-10'>
+        <p className='text-center text-3xl text-indigo-400 hover:underline'><Link href="/watch">Watch Page</Link></p>
+        <p className='text-center text-3xl text-indigo-400 hover:underline'><Link href="/channel">Channel List</Link></p>
+        <p className='text-center text-3xl text-indigo-400 hover:underline'><Link href="/channel/id-insert-here">Channel Details</Link></p>
+      </div>
     </div>
   )
 }
