6620cb4d616ca8b746f1616a73f4701068554a10
quadseed <contact@quadseed.net>
2022-06-26 15:52:34 +0900

    Edit: add channel list

--- pages/channel/index.tsx
+++ pages/channel/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
 import Header from '../../components/Header';
+import { testData } from '../../components/test';
 
 
 const Index: NextPage = () => {
@@ -12,7 +13,21 @@ const Index: NextPage = () => {
       </Head>
       
       <Header />
-      Index
+      
+      <div className='items-center px-2 py-5 justify-center'>
+          {testData.channelList.map((item) => (
+            <div className='flex my-10 items-center justify-center space-y-2 sm:mx-2 md:mx-24 lg:mx-44 xl:mx-80 2xl:mx-96'>
+              <img className='flex rounded-full hover:cursor-pointer h-14 w-14 md:h-20 md:w-20' src={item.icon} />
+              <div className='px-3'>
+                <p className='text-xl font-semibold md:font-bold hover:cursor-pointer'>{item.channelName}</p>
+                <p className='text-sm md:text-lg'>{item.subscriber}</p>
+                <p className='line-clamp-1 text-gray-500 text-xs md:text-sm my-1' >{item.description}</p>
+              </div>
+            </div>
+
+          ))}
+          
+        </div>
     </div>
   )
 }
