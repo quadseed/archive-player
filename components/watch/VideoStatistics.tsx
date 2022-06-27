2b6bf1eabf3ece0cefe99f51525907b7e328d7fc
quadseed <contact@quadseed.net>
2022-06-28 01:45:23 +0900

    Edit: change some style

--- components/watch/VideoStatistics.tsx
+++ components/watch/VideoStatistics.tsx
@@ -5,22 +5,21 @@ import IconBuilder from '../Utils';
 
 function VideoStatistics() {
 
-  const like =`${testData.like} Likes`;
-  const dislike =`${testData.dislike} Dislikes`;
+  const dislike = (testData as any).dislike ?? "Dislike";
   
   return (
     <div className='border-b-2'>
       <div className='flex items-center m-1 text-sm md:text-xl font-base'>{testData.title}</div>
-      <div className='flex space-x-20 md:space-x-60 space-y-1 text-sm'>
+      <div className='flex items-center space-y-1 text-sm'>
 
         <div className='flex items-center space-x-5 my-1'>
           <p>{testData.views.toLocaleString()} views</p>
           <p>published at {testData.date}</p>
         </div>
 
-        <div className='flex pl-10 2xl:pl-96 space-x-5 space-y-1'>
+        <div className='flex items-center pl-10 space-x-5 space-y-1'>
             <div className='text-green-600'>
-            <IconBuilder Icon={ThumbUpIcon} title={like} />
+            <IconBuilder Icon={ThumbUpIcon} title={testData.like} />
             </div>
             <div className='text-red-500'>
             <IconBuilder Icon={ThumbDownIcon} title={dislike} />
