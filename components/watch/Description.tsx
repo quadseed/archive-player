af01f4816aa810b8665d2c289cbc70f3313af859
quadseed <contact@quadseed.net>
2022-06-28 01:44:27 +0900

    Edit: change some style

--- components/watch/Description.tsx
+++ components/watch/Description.tsx
@@ -7,14 +7,14 @@ function Description() {
   return (
     <div className='mx-3 my-4 pb-3 border-b-2 space-y-4'>
       <div className='flex items-center px-2 py-1'>
-        <Link href="/channel"><img className='flex h-14 w-14 rounded-full hover:cursor-pointer' src={testData.icon} /></Link>
+        <Link href={"/channel/" + testData.channelId}><img className='flex h-14 w-14 rounded-full hover:cursor-pointer' src={testData.icon} /></Link>
         <div className='px-3'>
-          <p className='text-sm font-bold hover:underline hover:cursor-pointer'><Link href="/channel">{testData.channelName}</Link></p>
-          <p className='text-xs'>{testData.subscriber}</p>
+          <p className='text-sm font-bold hover:underline hover:cursor-pointer'><Link href={"/channel/" + testData.channelId}>{testData.channelName}</Link></p>
+          <p className='text-xs'>{testData.subscriber.toLocaleString()} Subscribers</p>
         </div>
       </div>
 
-      <div className='mx-10'>
+      <div className='mx-10 pl-9'>
       <MultiLineString body={testData.description} />
       </div>
     </div>
