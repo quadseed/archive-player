da39794fb25c2592c79ec5ce3a4fcae9de50e003
quadseed <contact@quadseed.net>
2022-06-29 10:37:04 +0900

    Edit: change some params

--- components/watch/Description.tsx
+++ components/watch/Description.tsx
@@ -5,7 +5,7 @@ import { MultiLineString } from '../Utils'
 
 function Description() {
   return (
-    <div className='mx-3 my-4 pb-3 border-b-2 space-y-4'>
+    <div className='mx-3 my-4 pb-5 border-b-2 space-y-4'>
       <div className='flex items-center px-2 py-1'>
         <Link href={"/channel/" + testData.channelId}><img className='flex h-14 w-14 rounded-full hover:cursor-pointer' src={testData.icon} /></Link>
         <div className='px-3'>
--- pages/watch.tsx
+++ pages/watch.tsx
@@ -22,7 +22,7 @@ const Watch: NextPage = () => {
 
         <div className='xl:w-3/4 space-y-5'>
           <Player url={testData.url} thumbnailUrl={testData.thumbnailUrl} />
-          <div className='px-10 py-2 md:px-32 md:py-4'>
+          <div className='px-10 py-2 lg:px-32 md:py-4'>
             <VideoStatistics />
             <Description />
             <Comments />
