8e85ef83ebfced8f72f0a020eabf8a8acaf0bb12
quadseed <contact@quadseed.net>
2022-06-30 20:26:01 +0900

    Edit: change icon size

--- components/Utils.tsx
+++ components/Utils.tsx
@@ -20,8 +20,8 @@ export const MultiLineString = ({ body }: { body: string }) => {
 function IconBuilder({Icon, title}: Props) {
   return (
     <div className='flex items-center space-x-1 p-1'>
-      <Icon className='h-5 w-5' />
-      <p>{title}</p>
+      <Icon className='h-4 w-4' />
+      <p className='text-sm'>{title}</p>
     </div>
   )
 }
